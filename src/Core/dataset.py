d3854cc2e8a2b1d44c434fd5543e607c8213bd3b
Michael Kupperman <kupperma@uw.edu>
2021-06-02 18:20:58 -0700

    Bugfixes for tokenized data input support.

--- src/Core/dataset.py
+++ src/Core/dataset.py
@@ -5,41 +5,46 @@ import copy
 import math
 
 
-class Dataset(torch.utils.data.Dataset):
+class Dataset(torch.utils.data.IterableDataset):
     # embed map should be built once per application and shared between classes/datasets
-    def __init__(self, data_triples, token_len: int = 1, dense=False, embed=None, debed=None):
+    def __init__(self, data_dict, embed):#  token_len: int = 1, dense=False, embed=None, debed=None):
         super().__init__()
         self.full_data = []
         self.data_list = []
         self.label_list = []
-        if embed is None and debed is None:
-            self.embedding, self.debedding = generate_embedding_map(token_len, dense=dense)
-        else:
-            self.embedding = embed
-            self.debedding = debed
+        self.embedding = embed
+        # if embed is None and debed is None:
+        #    self.embedding, self.debedding = generate_embedding_map(token_len, dense=dense)
+        # else:
+        #    self.embedding = embed
+        #    self.debedding = debed
 
-        for data_item in data_triples:
-            label_embed = np.asarray(tokenize_seq(data_item[2]))
-            data_embed = np.asarray(onehot_tokens(tokenize_seq(data_item[1]), embed_map=self.embedding))
-            self.full_data.append({'key': data_item[0],
-                                   'seq': data_item[1],
-                                   'label_seq': data_item[2],
+        for idx in range(len(data_dict['aligned_seqs'])):
+            # label_embed = np.asarray(tokenize_seq(data_item[2]))
+            # data_embed = np.asarray(onehot_tokens(tokenize_seq(data_item[1]), embed_map=self.embedding))
+
+            self.full_data.append({'key': data_dict['accession_id'][idx],
+                                   'seq': data_dict['aligned_seqs'][idx],
+                                   'label_seq': data_dict['labels'][idx],
                                    # Now we tokenize the label and input string to dense numbered vectors
-                                   'label': label_embed,
-                                   'tokens': data_embed})
-            self.data_list.append(data_embed)
-            self.label_list.append(label_embed)
+                                   'tokens': data_dict['numerical_tokens'][idx]})
+            self.data_list.append(torch.tensor(data_dict['numerical_tokens'][idx]))
+            self.label_list.append(torch.tensor(data_dict['labels'][idx]))
 
         self.start = 0
         self.end = len(self.full_data)
+        self.eval_mode = True
 
     def embedding_size(self):
         return len(self.embedding.keys())
 
     def return_dataset_pytorch(self, batch_size=32):
-        dataset = torch.utils.data.DataLoader(Dataset=self,  # A neat self-reference!
-                                              pin_memory=True, batch_size=batch_size, prefetch_workers=4)
+        dataset = torch.utils.data.DataLoader(dataset=self,  # A neat self-reference!
+                                              pin_memory=True, batch_size=batch_size)
         return dataset
+
+    def __len__(self):
+        return self.end
 
     def __iter__(self):
         worker_info = torch.utils.data.get_worker_info()
@@ -56,7 +61,7 @@ class Dataset(torch.utils.data.Dataset):
         else:
             return iter(self.full_data[iter_start:iter_end])
 
-    def share_embed_debed(self):
-        # Returns a deep copy of the embedding and debedding maps
-        return copy.deepcopy(self.embedding), copy.deepcopy(self.debedding)
+    def share_embed(self):
+        # Returns a deep copy of the embedding map
+        return copy.deepcopy(self.embedding)
 
